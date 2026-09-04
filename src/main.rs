351348cd9466e38ae82da6d8031aed30eb563ed7
watch-r <nabil202051@gmail.com>
2026-09-04 20:17:51 +0600

    Rename project to mntisp and implement CLI structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "mntisp")]
+#[command(about = "Manages the rclone ISP Media mount")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+#[derive(Subcommand)]
+enum Commands {
+    /// Mounts the ISP Media share
+    Start,
+    /// Unmounts the ISP Media share
+    Stop,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Start => println!("You picked Start"),
+        Commands::Stop => println!("You picked Start"),
+    }
 }
